4f9ca2d94e482d0ad55fbe777abddc44650a2878
GabrielAssumpcaoLiz <gabriel.liz@univille.br>
2021-06-10 14:47:37 -0300

    Projeto para fazer o registro de ponto

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from "@angular/material/button";
+
+import { AppComponent } from './app.component';
+
+import { LoginModule, 
+         LoginRoutingModule, 
+         CadastroPjModule,
+         CadastroPjRoutingModule
+         } from './autenticacao';
+
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    LoginModule,
+    LoginRoutingModule,
+    CadastroPjModule,
+    CadastroPjRoutingModule,
+
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
